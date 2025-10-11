f7328ac978459e4707d186f561c26c7ae4f0e765
brittany-potato <brittanycc@missionreadyhq.com>
2025-10-11 16:05:56 +1300

    Created Main Sourcecode file blueprint

---
+++ Program.cs
@@ -0,0 +1,20 @@
+//* Contains the 'Main' method, which is the entry point in most C# applications.
+
+// 'using' directives bring in namespaces, which are collections of related code.
+// 'system' namespace contains fundamental classes and base types.
+using System;
+
+// A namespace is used to organize your code and prevent naming conflicts.
+namespace MyFirstCSharpApp
+{
+    // A class is a blueprint for creating objects.
+    class Program
+    {
+        // The Main method is the starting point of your application.
+        static void Main(string[] args)
+        {
+            // Console.WriteLine() is a Method that prints text to console.
+            Console.WriteLine("Hello, C# World");
+        }
+    }
+}
